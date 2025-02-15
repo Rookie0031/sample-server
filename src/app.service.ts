2c8b8f28f6ca2bcc8b219f48fdb24f67e6c3cd7f
Jay <103009135+Rookie0031@users.noreply.github.com>
2025-02-15 18:56:08 +0900

    remove current folders

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Jisu! version3';
+    return 'Hello World! Jisu! version4';
   }
 
   getUserInfo(id: string): object {
